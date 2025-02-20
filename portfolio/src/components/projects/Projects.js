1b0f0a0e5e5d68ece529b8ae8f3ba11a9dc7ef0a
Marcos Gabriel <marcosindev@gmail.com>
2025-02-20 15:12:19 -0300

    Atualizações

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -2,7 +2,7 @@ import styles from './Projects.module.css'
 
 import project1 from '../../img/Captura de tela 2025-01-13 140455.png'
 import project2 from '../../img/Captura de tela 2025-01-13 140519.png'
-import project3 from  '../../img/Captura de tela 2025-01-13 152015.png'
+import project3 from '../../img/Captura de tela 2025-02-20 151054.png'
 import project4 from '../../img/Captura de tela 2025-01-13 152527.png'
 
 import Cards from './cards/cards'
@@ -17,9 +17,9 @@ function Projects() {
                 </h1>
             </div>
             <div className={styles.content}>
-                <Cards img={project1} name='Pokedex' code='https://github.com/Marquiiim/Pokedex' hosting='https://marquiiim.github.io/Pokedex/' tec='JS, React, CSS, HTML, Sass' />
+                <Cards img={project1} name='Pokedex' code='https://github.com/Marquiiim/Pokedex' hosting='https://marquiiim.github.io/pokedex/' tec='JS, React, CSS, HTML, Sass' />
                 <Cards img={project2} name='Loja de celular' code='https://github.com/Marquiiim/loja-iphone' hosting='https://marquiiim.github.io/loja-iphone/' tec='JS, React, CSS, HTML, Sass' />
-                <Cards img={project3} name='To-Do-List' code='https://github.com/Marquiiim/To-Do-List' hosting='https://marquiiim.github.io/To-Do-List/' tec='JS, React, CSS, HTML, Sass' />
+                <Cards img={project3} name='Costs' code='https://github.com/Marquiiim/costs-project' hosting='https://marquiiim.github.io/costs-project/' tec='JS, React, CSS, HTML, Sass' />
                 <Cards img={project4} name='Portfolio Antigo' code='https://github.com/Marquiiim/portfolio' hosting='https://marquiiim.github.io/portfolio/' tec='JS, CSS, HTML' />
             </div>
         </section>
