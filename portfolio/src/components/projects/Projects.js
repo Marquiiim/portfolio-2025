efa74a5b748e94c6e4a610921bcdea5af8efde5b
Marcos Gabriel <marcosindev@gmail.com>
2026-02-25 17:56:36 -0300

    adjustment sass & component structure

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -1,66 +1,82 @@
 import styles from './Projects.module.css'
-
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
-
-import { useState } from 'react'
-
-import LuxureStore from '../../img/Luxure_Store.png'
-import Concessionaria from '../../img/Loja_Veiculos.png'
-import CD342 from '../../img/CD_342.png'
-import Barbearia from '../../img/Barbearia.png'
-
+import { useState, useEffect } from 'react'
 import Cards from './cards/cards'
 
 function Projects() {
-
     const [currentIndex, setCurrentIndex] = useState(0)
+    const itemsPerPage = 3
 
-    const items = [
-        <Cards img={LuxureStore} name='Projeto Luxure Store' code='https://github.com/Marquiiim/luxure-store' hosting='' tec='JS, React, CSS, HTML, SASS' />,
-
-        <Cards img={Concessionaria} name='Projeto AutoManager' code='https://github.com/Marquiiim/loja-veiculos' hosting='' tec='JS, CSS, HTML, SASS, React, MySQL, Express, Axios' />,
-
-        <Cards img={Barbearia} name='Projeto BarberControl' code='https://github.com/Marquiiim/marcos-barbearia' tec='JS, CSS, HTML, SASS, React, MySQL, Express, Axios' />,
-
-        <Cards img={CD342} name='Projeto PatrimonyControl' code='https://github.com/Marquiiim/Sistema-CD-342' tec='JS, CSS, HTML, SASS, React, MySQL, Express, Axios' />,
+    const projectsData = [
+        { name: 'Projeto Luxure Store', code: 'https://github.com/Marquiiim/luxure-store', hosting: '', tec: 'JS, React, SASS' },
+        { name: 'Projeto AutoManager', code: 'https://github.com/Marquiiim/loja-veiculos', hosting: '', tec: 'JS, SASS, React, MySQL, Express, Axios' },
+        { name: 'Projeto BarberControl', code: 'https://github.com/Marquiiim/marcos-barbearia', tec: 'JS, SASS, React, MySQL, Express, Axios' },
+        { name: 'Projeto PatrimonyControl', code: 'https://github.com/Marquiiim/Sistema-CD-342', tec: 'JS, SASS, React, MySQL, Express, Axios' },
+        { name: 'Projeto TMSystem', code: 'https://github.com/Marquiiim/Sistema-CD-342', tec: 'JS, SASS, React, MySQL, Express, Axios' },
     ]
 
+    const totalItems = projectsData.length
+    const maxIndex = Math.max(0, totalItems - itemsPerPage)
+
+    useEffect(() => {
+        if (currentIndex > maxIndex) {
+            setCurrentIndex(maxIndex)
+        }
+    }, [currentIndex, maxIndex])
+
     const next = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 3) % items.length)
+        setCurrentIndex(prev => Math.min(prev + itemsPerPage, maxIndex))
     }
 
     const prev = () => {
-        setCurrentIndex((prevIndex) => prevIndex === 0 ? items.length - 3 : prevIndex - 3)
+        setCurrentIndex(prev => Math.max(prev - itemsPerPage, 0))
     }
 
     const getVisibleItems = () => {
-        const endIndex = currentIndex + 3
-        if (endIndex > items.length) {
-            return [...items.slice(currentIndex), ...items.slice(0, endIndex % items.length)]
-        } else {
-            return items.slice(currentIndex, endIndex)
-        }
+        return projectsData.slice(currentIndex, currentIndex + itemsPerPage)
     }
+
+    const visibleItems = getVisibleItems()
 
     return (
         <section className={styles.container} id='projects'>
             <div>
-                <h1>
-                    Projetos
-                </h1>
+                <h1>Projetos</h1>
             </div>
+
             <div className={styles.content}>
-                <button onClick={prev} className={styles.arrow_left}>
+                <button
+                    onClick={prev}
+                    className={`${styles.arrow_left} ${currentIndex === 0 ? styles.disabled : ''}`}
+                    disabled={currentIndex === 0}
+                >
                     <FiArrowLeft />
                 </button>
+
                 <div className={styles.carousel}>
-                    {getVisibleItems().map((item, index) => (
-                        <div key={index} style={{ flex: '0 0 33.33%' }}>
-                            {item}
+                    {visibleItems.length > 0 ? (
+                        visibleItems.map((project, index) => (
+                            <div key={`${currentIndex}-${index}`} className={styles.carouselItem}>
+                                <Cards
+                                    name={project.name}
+                                    code={project.code}
+                                    hosting={project.hosting}
+                                    tec={project.tec}
+                                />
+                            </div>
+                        ))
+                    ) : (
+                        <div className={styles.emptyState}>
+                            <p>Nenhum projeto disponível</p>
                         </div>
-                    ))}
+                    )}
                 </div>
-                <button onClick={next} className={styles.arrow_right} >
+
+                <button
+                    onClick={next}
+                    className={`${styles.arrow_right} ${currentIndex >= maxIndex ? styles.disabled : ''}`}
+                    disabled={currentIndex >= maxIndex}
+                >
                     <FiArrowRight />
                 </button>
             </div>
